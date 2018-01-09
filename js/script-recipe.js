72a6ea490e8a076efbf6ada6c0392bfe7033515a
adgalaz <adgalaz@gmail.com>
2018-01-08 22:49:25 -0300

    actividades 7,8,9 y 10

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -5,6 +5,42 @@ $(document).ready(function () {
   */
   removeBack();
 
+  /*
+  * Etapa 7
+  * Imprimiendo mensaje en la consola cuando documento esta listo
+  */
+  console.log('Hello!! I am ready!!')
+
+
+  /*
+  * Etapa 8 - Etapa 9
+  * Agregando y quitando clase make
+  * Agregando y quitando clase active
+  */
+  $('.js-show-recipe').click(function(){
+    console.log("btn recipe");
+        $('.page').removeClass('make');
+        $(this).addClass('active');
+        $('.js-show-make').removeClass('active');
+    });
+
+  $('.js-show-make').click(function(){
+      console.log("btn make it");
+        $('.page').addClass('make');
+        $(this).addClass('active');
+        $('.js-show-recipe').removeClass('active');
+    });
+
+    /*
+    * Etapa 10
+    * Agregando href a botón back
+    */
+
+    $('.js-back').click(function(){
+      console.log("btn back");
+        $('.js-back').attr("href", "index.html");
+    });
+
 });
 
 /*
